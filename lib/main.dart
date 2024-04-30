bf5ee9edc976eb35c35f33414d106d9fe21877bc
coderaminulislam <aminulwife.taslima@gmail.com>
2024-05-01 00:19:30 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
   runApp(profileApp());
@@ -8,7 +9,8 @@ void main() {
 class profileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
+
       debugShowCheckedModeBanner: false,
       home: ProfileActivity(),
     );
@@ -21,59 +23,127 @@ class ProfileActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffFCEAEAFF),
         appBar: AppBar(
           centerTitle: true,
-          title: Text('Profile'),
+          title: const Text('Profile'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/aminul.jpg'))),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Md Aminul Islam',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  fontSize: 22,
-                ),
-              ),
-              Text(
-                'mdaminulifno@gmail.com',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black.withOpacity(.6),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Text(
-                  textAlign: TextAlign.center,
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300
+        body: OrientationLayoutBuilder(
+          portrait: (context) => SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50,),
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: const DecorationImage(
+                              image: AssetImage('assets/images/aminul.jpg'))),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Md Aminul Islam',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        fontSize: 22,
+                      ),
+                    ),
+                    Text(
+                      'mdaminulifno@gmail.com',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black.withOpacity(.6),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400
 
-                  ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
+              ),
+            ),
+          ) ,
+          landscape: (context)=>SingleChildScrollView(
+            child: Center(
+              child: Container(
+                width: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50,),
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: const DecorationImage(
+                              image: AssetImage('assets/images/aminul.jpg'))),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Md Aminul Islam',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        fontSize: 22,
+                      ),
+                    ),
+                    Text(
+                      'mdaminulifno@gmail.com',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black.withOpacity(.6),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400
+
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
-        ));
+
+        )
+    );
+
   }
 }
